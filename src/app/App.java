d1cb72f27d243dc8c7d833c48a0867d6ded60aa4
Author Name <hire@yuxuan.cloud>
2021-03-25 21:25:12 +0000

    My first commit on Theia.

---
+++ src/app/App.java
@@ -0,0 +1,8 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String myVar = "I love Theia IDE";
+        System.out.println("Hello Java");
+    }
+}
